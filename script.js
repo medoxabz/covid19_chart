d7c33c540784c06ce9c09a4182cb13a8d7fdf557
mohammed <medoxabz@gmail.com>
2022-12-07 21:15:10 +0100

    modifier script.js

--- script.js
+++ script.js
@@ -1,54 +1,62 @@
 
+
 let sidebar=document.querySelector(".content_side");
 const ctx = document.getElementById('myChart');
 
+function updateChart(Chart,res){
+  let labels = res.map(e=>{
+    let d=new Date(e.Date);
+    day=d.getDate();
+    month=d.getMonth()+1;
+    return `${day}/${month}`;
+   });
+
+ 
+   let confirmed = res.map(e=>{return e.Confirmed});
+   let recovered = res.map(e=>{return e.Recovered});
+   let Active = res.map(e=>{return e.Active});
+   
+   let Deaths = res.map(e=>{return e.Deaths});
+   let dataset=[
+    {
+        label:"confirmed",
+        data:confirmed,
+        borderColor:"red",
+    },
+    {
+      label:"Recovered",
+      data:recovered,
+      borderColor:"blue",
+    },
+    {
+     label:"Active",
+     data:Active,
+     borderColor:"green",
+   },
+   {
+     label:"Deaths",
+     data:Deaths,
+     borderColor:"yellow",
+   },
+   ];
+   myChart.data.labels=labels;
+   myChart.data.datasets=dataset;
+   myChart.update();
+}
+
 function countryClicked(element){
-  let code=element.target.getAttribute("id");
+  if(element){
+      let code=element.target.getAttribute("id");
+  }
+  else code="MA"
    let newXhr = new XMLHttpRequest();
    newXhr.open("Get","https://api.covid19api.com/dayone/country/"+ code,true);
 
    newXhr.onreadystatechange=()=>{
      if(newXhr.status==200 && newXhr.readyState==4){
     var res = JSON.parse(newXhr.response);
-      console.log(res);
-    let labels = res.map(e=>{
-     let d=new Date(e.Date);
-     day=d.getDate();
-     month=d.getMonth()+1;
-     return `${day}/${month}`;
-    });
-
-  
-    let confirmed = res.map(e=>{return e.Confirmed});
-    let recovered = res.map(e=>{return e.Recovered});
-    let Active = res.map(e=>{return e.Active});
-    
-    let Deaths = res.map(e=>{return e.Deaths});
-    let dataset=[
-     {
-         label:"confirmed",
-         data:confirmed,
-         borderColor:"red",
-     },
-     {
-       label:"Recovered",
-       data:recovered,
-       borderColor:"blue",
-     },
-     {
-      label:"Active",
-      data:Active,
-      borderColor:"green",
-    },
-    {
-      label:"Deaths",
-      data:Deaths,
-      borderColor:"yellow",
-    },
-    ];
-    myChart.data.labels=labels;
-    myChart.data.datasets=dataset;
-    myChart.update();
+      
+        updateChart(myChart,res);
    
           }
    }
@@ -69,7 +77,7 @@ xhr.onreadystatechange=()=>{
             element.appendChild(txt);
             element.classList.add("country");
             sidebar.appendChild(element);
-
+            countryClicked(null);
           
 
             element.addEventListener("click",countryClicked);
